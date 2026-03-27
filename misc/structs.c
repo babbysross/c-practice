54929b6a17985fa17b0d0f58177186ca838e4d38
solisares <rsg.elias@gmail.com>
2026-03-27 18:23:15 +0000

    fixed structs.c

--- misc/structs.c
+++ misc/structs.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
 typedef struct {
-    char name;
+    char *name;
     int age;
-} boys[10] =  {
-    "Freddie", 20,
-    "Jonas", 15,
-    "Billy", 18
+} Stats; 
+
+Stats boys[] =  {
+    {"Freddie", 20},
+    {"Jonas", 15},
+    {"Billy", 18}
 };
 
 int main() {
-    printf("%s is %d years old.", boys[0],boys[1]);
+    printf("%s is %d years old.\n", boys[0].name,boys[0].age);
     return 0;
 }
